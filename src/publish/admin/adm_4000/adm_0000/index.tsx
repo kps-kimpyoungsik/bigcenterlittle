42acbe6a856daeef94ae6c96be91e0b649f1646d
spacepublisher <spacepublisher@gmail.com>
2024-04-16 15:52:45 +0900

    버튼 컴포넌트 수정2
    버튼 컴포넌트 수정2

--- src/publish/admin/adm_4000/adm_0000/index.tsx
+++ src/publish/admin/adm_4000/adm_0000/index.tsx
@@ -14,30 +14,30 @@ function ContentsContainer() {
 
 					<br></br>
 					<div className="hz-root hz-gap10">
-						<Button >버튼 hz</Button>
-						<Button color="pri-o" size="big" hz="">버튼</Button>
-						<Button color="pri-o2" size="big" hz="" >버튼</Button>
-						<Button color="gray" size="big" hz="" >버튼</Button>
-						<Button color="gray-o" size="big" hz="" >버튼</Button>
+						<Button color="pri" size="big">버튼</Button>
+						<Button color="pri-o" size="big">버튼</Button>
+						<Button color="pri-o2" size="big">버튼</Button>
+						<Button color="gray" size="big">버튼</Button>
+						<Button color="gray-o" size="big">버튼</Button>
 					</div>
 
 					<br></br>
 					<div className="hz-root hz-gap8">
-						<Button color="pri" size="md" hz="" >버튼</Button>
-						<Button color="pri-o" size="md" hz="" >버튼</Button>
-						<Button color="pri-o2" size="md" hz="" >버튼</Button>
-						<Button color="gray" size="md" hz="" >버튼</Button>
-						<Button color="gray-o" size="md" hz="" >버튼</Button>
+						<Button color="pri" size="md">버튼</Button>
+						<Button color="pri-o" size="md">버튼</Button>
+						<Button color="pri-o2" size="md">버튼</Button>
+						<Button color="gray" size="md">버튼</Button>
+						<Button color="gray-o" size="md">버튼</Button>
 					</div>
 
 					<br></br>
 					<div className="hz-root hz-gap5">
-						<Button color="pri" size="sm" hz="" >버튼</Button>	
-						<Button color="pri-o" size="sm" hz="" >버튼</Button>	
-						<Button color="pri-o2" size="sm" hz="" >버튼</Button>	
-						<Button color="gray" size="sm" hz="" >버튼</Button>	
-						<Button color="gray-o" size="sm" hz="" >버튼</Button>	
-						<Button color="gray-o" size="sm" leftIcon  hz="">버튼</Button>	
+						<Button color="pri" size="sm">버튼</Button>	
+						<Button color="pri-o" size="sm">버튼</Button>	
+						<Button color="pri-o2" size="sm">버튼</Button>	
+						<Button color="gray" size="sm">버튼</Button>	
+						<Button color="gray-o" size="sm">버튼</Button>	
+						<Button color="gray-o" size="sm">버튼</Button>	
 					</div>
 				</main>			
 			</div>
