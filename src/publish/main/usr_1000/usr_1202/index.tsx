059e86624b79bf892cae6c89b5f88262d519dfba
JI-jeong <jijeong119@gmail.com>
2024-04-17 19:13:45 +0900

    사용자화면 ID적용

---
+++ src/publish/main/usr_1000/usr_1202/index.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Link } from 'react-router-dom';
+import PageContainer from '../../PageContainer';
+import Button from 'components/buttons/';
+function ContentsContainer() {
+	return (
+		<PageContainer>
+			<div className='fww-con'>
+				page
+			</div>			
+		</PageContainer>
+	);
+}
+export default ContentsContainer;
