9eb23ad4a46a9a65ba300b6b92c4a45a70f5bf07
JI-jeong <jijeong119@gmail.com>
2024-04-18 15:51:39 +0900

    modalsize 수정

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -5,7 +5,7 @@ import ReactModal from 'react-modal';
 type ModalPopupProps = {
 	open: boolean;
 	title: string;
-	size: 'sm' | 'mid' | 'lg' | 'alert';
+	size: 'sm' | 'mid' | 'lg' | 'xlg' | 'alert';
 	onClose?: () => void;
 	children?: React.ReactNode;
 };
