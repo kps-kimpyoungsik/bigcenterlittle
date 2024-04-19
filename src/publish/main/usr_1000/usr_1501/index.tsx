eba693a0fcfc336a5c172861d20f769de4c5b174
JI-jeong <jijeong119@gmail.com>
2024-04-19 10:51:51 +0900

    사용자 비밀번호수정

--- src/publish/main/usr_1000/usr_1501/index.tsx
+++ src/publish/main/usr_1000/usr_1501/index.tsx
@@ -6,6 +6,12 @@ import Button from 'components/buttons/';
 import UIInput from 'components/input/input';
 import UIDatePicker from "components/datepicker";
 function ContentsContainer() {
+	const sectors = [
+		'농업, 임업 및 어업', '광업', '제조업', '전기·가스 증기 및 수도사업', '하수 · 폐기물 처리, 원료재생 및 환경복원업', '건설업', '도매 및 소매업', '운수업', 
+		'숙박 및 음식점업', '출판, 영상, 방송통신 및 정보서비스업',  '금융 및 보험업','부동산업 및 임대업',	'전문, 과학 및 기술 서비스업',	'공공행정, 국방 및 사회보장 행정',
+		'교육 서비스업', '보건업 및 사회복지 서비스업', '예술, 스포츠 및 여가관련 서비스업','협회 및 단체, 수리 및 기타 개인 서비스업',	
+		'가구내 고용활동 및 달리 분류되지 않은 자가소비 생산활동','국제 및 외국기관','기타'
+	];
 	return (
 		<PageContainer>
 			<div className='fww-con'>
@@ -39,29 +45,10 @@ function ContentsContainer() {
 									<div className="fo-value">
 										<div className="sl sl-md">
 											<select>
-												<option>업종 </option>
-												<option>건설업 </option>
-												<option>농업, 임업 및 어업</option>
-												<option>광업</option>
-												<option>제조업</option>
-												<option>전기·가스 증기 및 수도사업</option>
-												<option>하수 · 폐기물 처리, 원료재생 및 환경복원업</option>
-												<option>건설업</option>
-												<option>도매 및 소매업</option>
-												<option>운수업</option>
-												<option>숙박 및 음식점업</option>
-												<option>출판, 영상, 방송통신 및 정보서비스업</option>
-												<option>금융 및 보험업</option>
-												<option>부동산업 및 임대업</option>
-												<option>전문, 과학 및 기술 서비스업</option>
-												<option>공공행정, 국방 및 사회보장 행정</option>
-												<option>교육 서비스업</option>
-												<option>보건업 및 사회복지 서비스업</option>
-												<option>예술, 스포츠 및 여가관련 서비스업</option>
-												<option>협회 및 단체, 수리 및 기타 개인 서비스업</option>
-												<option>가구내 고용활동 및 달리 분류되지 않은 자가소비 생산활동</option>
-												<option>국제 및 외국기관</option>
-												<option>기타</option>
+												<option value="">업종</option>
+												{sectors.map((sec, idx) =>
+													<option key={idx}>{sec}</option>
+												)}
 											</select>
 										</div>
 									</div>
@@ -69,16 +56,18 @@ function ContentsContainer() {
 								<div className="fo-item req">
 									<p className="fo-key">설립일자</p>
 									<div className="fo-value">
-										<UIDatePicker />
+										<div className="w100p">
+											<UIDatePicker />
+										</div>
 									</div>
 								</div>
 								<div className="fo-item req">
 									<p className="fo-key">설립형태</p>
 									<div className="fo-value">
-									<div className="sl sl-md">
+										<div className="sl sl-md">
 											<select>
 												<option>영리</option>
-												<option selected>비영리</option>
+												<option>비영리</option>
 											</select>
 										</div>
 									</div>
@@ -89,7 +78,7 @@ function ContentsContainer() {
 										<div className="sl sl-md">
 											<select>
 												<option>대기업</option>
-												<option selected>공공기관</option>
+												<option>공공기관</option>
 												<option>중견기업</option>
 												<option>중소기업</option>
 												<option>기타</option>
@@ -110,15 +99,32 @@ function ContentsContainer() {
 									</div>
 								</div>
 								<div className="fo-item fo-m-1-3 req">
+									<p className="fo-key">아이디</p>
+									<div className="fo-value">
+										<div className="w-half">
+											<UIInput className={'tf tf-md w100p'} value={'Idshowing'} placeholder={''} readOnly={true} />
+										</div>
+									</div>
+								</div>
+								<div className="fo-item fo-m-1-3 req">
 									<p className="fo-key">주소</p>
 									<div className="fo-value">
-										<UIInput className={'tf tf-md w100p'} value={'기업명 출력'} placeholder={''} readOnly={true} />
+										<div className="address">
+											<div className="hz-root hz-gap8">
+												<UIInput className={'hz-item hz-fg tf tf-md'} value={'00000'} placeholder={''} disabled={true} />
+												<Button color='gray-o' size="md">주소찾기</Button>
+											</div>
+											<UIInput className={'tf tf-md w100p mt8'} value={'서울특별시 중구 퇴계로 173'} placeholder={''} disabled={true} />
+											<UIInput className={'tf tf-md w100p mt8'} value={'남산스퀘어빌딩 7층'} placeholder={''}/>
+										</div>
 									</div>
 								</div>
 								<div className="fo-item fo-m-1-3">
 									<p className="fo-key">홈페이지</p>
 									<div className="fo-value">
-										<UIInput className={'tf tf-md w100p'} value={'www.win-win.or.kr'} placeholder={''} />
+										<div className="w-half">
+											<UIInput className={'tf tf-md w100p'} value={'www.win-win.or.kr'} placeholder={''} />
+										</div>
 									</div>
 								</div>
 								<div className="fo-item fo-m-1-3 req">
--- src/publish/main/usr_1000/usr_1502/index.tsx
+++ src/publish/main/usr_1000/usr_1502/index.tsx
@@ -3,11 +3,49 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import PageContainer from '../../PageContainer';
 import Button from 'components/buttons/';
+import UIInputPW from 'components/input/inputPW';
 function ContentsContainer() {
 	return (
 		<PageContainer>
 			<div className='fww-con'>
-				page
+				<ul className="fww-brd">
+					<li><Link to="/main/">홈</Link></li>
+					<li><Link to="#">마이페이지</Link></li>
+					<li><Link to="#">비밀번호 수정</Link></li>
+				</ul>
+				<main className="fww-main">
+					<div className="mypage pw-edit">
+						<h1 className="tx tx-hd2">비밀번호 수정</h1>
+						<div className="sub-tx">비밀번호를 새롭게 설정해 주세요.</div>
+						<form className="fo fo-col2" name="" action="" method="">
+							<div className="inner">
+								<div className="fo-item fo-m-1-3">
+									<p className="fo-key">사용중이던 비밀번호를 입력</p>
+									<div className="fo-value">
+										<UIInputPW className={'tf tf-md w385'} value={''} placeholder={'비밀번호를 입력하세요'} />
+									</div>
+								</div>
+								<div className="fo-item fo-m-1-3">
+									<p className="fo-key">비밀번호를 한번 더 입력</p>
+									<div className="fo-value">
+										<UIInputPW className={'tf tf-md w385'} value={''} placeholder={'비밀번호를 한번 더 입력하세요.'} />
+									</div>
+								</div>
+								<div className="fo-item fo-m-1-3">
+									<p className="fo-key">새로운 비밀번호를 입력</p>
+									<div className="fo-value">
+										<UIInputPW className={'tf tf-md w385'} value={''} placeholder={'새로운 비밀번호를 입력하세요.'} />
+										<p className="tx-msg mt8">* 영문, 숫자, 특수문자를 혼합으로 8~16자리까지 입력 가능합니다.</p>
+									</div>
+								</div>
+							</div>
+						</form>
+						<div className="hz-root hz-center hz-gap10">
+							<Button color='gray-o' size="big">취소</Button>
+							<Button color='pri' size="big" href="/main/usr_1000/usr_1204">저장</Button>
+						</div>
+					</div>
+				</main>
 			</div>			
 		</PageContainer>
 	);
