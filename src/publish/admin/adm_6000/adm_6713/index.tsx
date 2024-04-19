1aa4626a66da8f85a43d1be8a813ca88e9c920c8
spacepublisher <spacepublisher@gmail.com>
2024-04-19 17:37:07 +0900

    6000 시스템관리 완료
    6000 시스템관리 완료

--- src/publish/admin/adm_6000/adm_6713/index.tsx
+++ src/publish/admin/adm_6000/adm_6713/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import PageContainer from '../../PageContainer';
 import LayoutAside from "../adm_6000Aside";
+import useToggleState from 'components/hooks/useToggleState';
+import ModalPopup from 'components/modal/';
 import Button from 'components/buttons/';
 import UIInput from 'components/input/input';
 function ContentsContainer() {
+	const modaladm6714 = useToggleState({});
+	const modaladm6715 = useToggleState({});
 	return (
 		<PageContainer>
 			<LayoutAside />
@@ -108,8 +112,141 @@ function ContentsContainer() {
 						<Button color="pri-o" size="sm">수정</Button>
 						<Button color="gray-o" size="sm">목록</Button>
 					</div>
+
+						{/* 실제 개발시 필요없는 버튼입니다. 삭제해주세요 */}
+						<Button color='gray' size="sm" onClick={modaladm6714.open}>adm_6714p 열기</Button> <br /><br />
+						<Button color='gray' size="sm" onClick={modaladm6715.open}>adm_6715p 열기</Button> <br /><br />
+						{/* 실제 개발시 필요없는 버튼입니다. 삭제해주세요 */}
+
 				</main>
 			</div>
+
+			{/* modal - 6714 */}
+			<ModalPopup
+				open={modaladm6714.isShowing}
+				title="모계좌관리 등록"
+				size="sm"
+				onClose={modaladm6714.close}
+			>
+				<div className="modal-con">
+				<form className="fo fo-col2" name="" action="" method="">
+						<div className="inner">
+							<div className="fo-item fo-m-1-3">
+								<p className="fo-key">펌뱅킹기관코드</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'00001234'} placeholder={''} readOnly={true} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3">
+								<p className="fo-key">은행</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'하나은행'} placeholder={''} readOnly={true} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">계좌번호</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={''} placeholder={'- 숫자만 입력'} readOnly={true} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">계좌별칭</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={''} placeholder={''} readOnly={false} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">대표계좌</p>
+								<div className="fo-value">
+									<div className="hz-root hz-gap15 h36">
+										<label htmlFor="rd-1-1" className="ip-rd-label">
+											<input name="rd-1" id="rd-1-1" className="ip-rd" type="radio"  />
+											<span className="check"></span>
+											<span className="txt">예</span>
+										</label>
+										<label htmlFor="rd-1-2" className="ip-rd-label">
+											<input name="rd-1" id="rd-1-2" className="ip-rd" type="radio"  checked />
+											<span className="check"></span>
+											<span className="txt">아니오</span>
+										</label>
+									</div>									
+								</div>
+							</div>
+
+						</div>
+					</form>
+				</div>
+				<div className="modal-bottom">
+				<div className="hz-root hz-gap8">
+						<Button color='gray-o' size="sm">취소</Button>
+						<Button color='pri' size="sm">저장</Button>
+					</div>
+				</div>	
+			</ModalPopup>		
+
+			{/* modal - 6715 */}
+			<ModalPopup
+				open={modaladm6715.isShowing}
+				title="모계좌관리 수정"
+				size="sm"
+				onClose={modaladm6715.close}
+			>
+				<div className="modal-con">
+				<form className="fo fo-col2" name="" action="" method="">
+						<div className="inner">
+							<div className="fo-item fo-m-1-3">
+								<p className="fo-key">펌뱅킹기관코드</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'00001234'} placeholder={''} readOnly={true} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3">
+								<p className="fo-key">은행</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'하나은행'} placeholder={''} readOnly={true} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">계좌번호</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'123-12-123456789'} placeholder={'- 숫자만 입력'} readOnly={false} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">계좌별칭</p>
+								<div className="fo-value">
+									<UIInput className={'tf tf-md w100p'} value={'대중소 상생기금 모계좌'} placeholder={''} readOnly={false} />
+								</div>
+							</div>
+							<div className="fo-item fo-m-1-3 req">
+								<p className="fo-key">대표계좌</p>
+								<div className="fo-value">
+									<div className="hz-root hz-gap15 h36">
+										<label htmlFor="rd-1-1" className="ip-rd-label">
+											<input name="rd-1" id="rd-1-1" className="ip-rd" type="radio"  />
+											<span className="check"></span>
+											<span className="txt">예</span>
+										</label>
+										<label htmlFor="rd-1-2" className="ip-rd-label">
+											<input name="rd-1" id="rd-1-2" className="ip-rd" type="radio"  checked />
+											<span className="check"></span>
+											<span className="txt">아니오</span>
+										</label>
+									</div>									
+								</div>
+							</div>
+
+						</div>
+					</form>
+				</div>
+				<div className="modal-bottom">
+				<div className="hz-root hz-gap8">
+						<Button color='gray-o' size="sm">취소</Button>
+						<Button color='pri' size="sm">저장</Button>
+					</div>
+				</div>	
+			</ModalPopup>					
+
 		</PageContainer>
 	);
 }
