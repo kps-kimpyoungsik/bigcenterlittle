2cf0a0ee3241919966c50742823889e9d1dd6771
JI-jeong <jijeong119@gmail.com>
2024-05-10 13:00:18 +0900

    Loading 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import './assets/styles/fww.css';
 import Main from "./publish/main/mainRoutes";
 import Admin from "./publish/admin/AdminRoutes";
+import NotFound from "./publish/norfound";
+import PageLoading from "./publish/loading";
 function App() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/main" render={ Main} />
-				<Route path="/admin" render={ Admin} />
-			</Switch>
-			<ToastContainer
-				position="top-right"
-				autoClose={3000}
-				draggable={false}
-				hideProgressBar
-				newestOnTop
-				closeOnClick
-				pauseOnHover
-			/>
-		</BrowserRouter>
+		<Suspense fallback={<PageLoading />}>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/main" render={ Main} />
+					<Route path="/admin" render={ Admin} />
+					<Route path="/*" render={NotFound} />
+				</Switch>
+				<ToastContainer
+					position="top-right"
+					autoClose={3000}
+					draggable={false}
+					hideProgressBar
+					newestOnTop
+					closeOnClick
+					pauseOnHover
+				/>
+			</BrowserRouter>
+		</Suspense>
 	);
 }
 export default App;
