dd3296f4863c375494848ffbafa19e205b17a6c2
spacepublisher <spacepublisher@gmail.com>
2024-05-10 13:50:42 +0900

    보고서(8000) 완료

--- src/publish/admin/adm_8000/adm_8100/index.tsx
+++ src/publish/admin/adm_8000/adm_8100/index.tsx
@@ -17,6 +17,82 @@ function ContentsContainer() {
 				</ul>
 				<main className="fww-main">
 					<h1 className="tx tx-hd2">누적 현황통계</h1>
+					<div className="hz-root hz-sb mb10">
+						<h3 className="section-title">연도별 현황(누적)</h3>
+						<div className="hz-item hz-root hz-gap5">
+							<Button color="gray-o" size="sm">접기</Button>
+							<Button color="gray-o" size="sm">펼치기</Button>
+							<Button color="gray-o" size="sm">다운로드</Button>
+						</div>
+					</div>
+					<div className="ibsheet mb40">[D] IBSHEET 영역</div>
+					
+
+					<div className="pt">
+						<div className="row">
+							<div className="col col-5 mr15">
+								<div className="hz-root hz-sb mb10">
+									<h3 className="section-title">상위출연 민간기업</h3>
+									<div className="hz-item hz-root hz-gap5">
+										<Button color="gray-o" size="sm">접기</Button>
+										<Button color="gray-o" size="sm">펼치기</Button>
+									</div>
+								</div>
+								<div className="ibsheet mb40">[D] IBSHEET 영역</div>
+							</div>
+							<div className="col col-5 ml15">
+								<div className="hz-root hz-sb mb10">
+									<h3 className="section-title">상위출연 공공기관</h3>
+									<div className="hz-item hz-root hz-gap5">
+										<Button color="gray-o" size="sm">접기</Button>
+										<Button color="gray-o" size="sm">펼치기</Button>
+									</div>
+								</div>		
+								<div className="ibsheet mb40">[D] IBSHEET 영역</div>						
+							</div>
+						</div>
+					</div>					
+
+					<div className="hz-root hz-sb mb10">
+						<h3 className="section-title">출연사업별 현황(누적)</h3>
+						<div className="hz-item hz-root hz-gap5">
+							<Button color="gray-o" size="sm">접기</Button>
+							<Button color="gray-o" size="sm">펼치기</Button>
+							<Button color="gray-o" size="sm">다운로드</Button>
+						</div>
+					</div>			
+					<div className="ibsheet mb40">[D] IBSHEET 영역</div>		
+
+					<div className="pt">
+						<div className="row">
+							<div className="col col-5 mr15">
+								<div className="hz-root hz-sb mb10">
+									<h3 className="section-title">기금사용용도(상생법)</h3>
+									<div className="hz-item hz-root hz-gap5">
+										<Button color="gray-o" size="sm">접기</Button>
+										<Button color="gray-o" size="sm">펼치기</Button>
+									</div>
+								</div>			
+								<div className="ibsheet mb40">[D] IBSHEET 영역</div>								
+							</div>
+							<div className="col col-5 ml15">
+								<div className="hz-root hz-sb mb10">
+									<h3 className="section-title">사업분야</h3>
+									<div className="hz-item hz-root hz-gap5">
+										<Button color="gray-o" size="sm">접기</Button>
+										<Button color="gray-o" size="sm">펼치기</Button>
+									</div>
+								</div>			
+								<div className="ibsheet mb40">[D] IBSHEET 영역</div>							
+							</div>
+						</div>
+					</div>					
+
+					<div className="hz-root hz-sb mb10">
+						<h3 className="section-title">주요성과</h3>
+					</div>		
+					<div className="ibsheet mb40">[D] IBSHEET 영역</div>
+
 				</main>
 			</div>
 		</PageContainer>
