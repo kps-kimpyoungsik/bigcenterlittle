26e2993cec79303c264f2bbd4d4cffc4fb6e886d
spacepublisher <spacepublisher@gmail.com>
2024-05-17 18:02:31 +0900

    Update adm_1000Aside.tsx

--- src/publish/admin/adm_1000/adm_1000Aside.tsx
+++ src/publish/admin/adm_1000/adm_1000Aside.tsx
@@ -4,7 +4,9 @@ import { Link, useLocation } from 'react-router-dom';
 function PortalAside() {
 	const aside = {
 		depth : {
-			sub2 : [
+			// 1.통합정보조회 --> 1depth
+			// 2.출연관리
+			sub2 : [ 
 				{
 					s_id:1,
 					s_name : "출연신청안내관리",
@@ -21,6 +23,7 @@ function PortalAside() {
 					s_path : "/admin/adm_1000/adm1212"
 				}
 			],
+			 // 3.과제관리
 			sub3 : [
 				{
 					s_id:1,
@@ -32,23 +35,9 @@ function PortalAside() {
 					s_name : "과제신청현황",
 					s_path : "/admin/adm_1000/adm1305"
 				},
-				{
-					s_id:3,
-					s_name : "부서장 과제심사",
-					s_path : "/admin/adm_1000/adm1308"
-				},
-				{
-					s_id:4,
-					s_name : "본부장 과제심사",
-					s_path : "/admin/adm_1000/adm1310"
-				},
-				{
-					s_id:5,
-					s_name : "과제관리대장",
-					s_path : "/admin/adm_1000/adm1313"
-				}
 			],
-			sub4 : [
+			// 4.지급관리
+			sub4 : [ 
 				{
 					s_id:1,
 					s_name : "지급신청안내관리",
@@ -59,24 +48,32 @@ function PortalAside() {
 					s_name : "지급신청현황",
 					s_path : "/admin/adm_1000/adm1405"
 				},
-				{
-					s_id:3,
-					s_name : "지급등록",
-					s_path : "/admin/adm_1000/adm1413"
-				}
 			],
-			sub5 : [
+			// 5.환수관리 --> 1depth로 변경
+			/*
+			sub5 : [ 
 				{
 					s_id:1,
 					s_name : "환수신청현황",
 					s_path : "/admin/adm_1000/adm1500"
 				},
+			],
+			*/
+			// 6.성과관리
+			// 7. 수요조사관리 --> 2depth로 변경
+			sub7: [ 
+				{
+					s_id:1,
+					s_name : "수요조사관리",
+					s_path : "/admin/adm_1000/adm1700"
+				},
 				{
 					s_id:2,
-					s_name : "환수현황",
-					s_path : "/admin/adm_1000/adm1503"
-				}
-			],
+					s_name : "수요조사모집현황",
+					s_path : "/admin/adm_1000/adm1704"
+				},
+			],			
+			// 8. 성과관리 --> 추가
 		}
 	}
 	const location = useLocation(); 
@@ -124,6 +121,7 @@ function PortalAside() {
 						))}
 					</ul>
 				</li>
+				{/*
 				<li>
 					<button className="ctr" type="button" onClick={(e)=>{toggleHandler(e)}}>환수관리</button>
 					<ul className='d2'>
@@ -134,19 +132,39 @@ function PortalAside() {
 						))}
 					</ul>
 				</li>
+				*/}
+				<li className={location.pathname.includes('adm15') ? 'active' : ''}>
+					<Link to="/admin/adm_1000/adm1500" >환수관리</Link>
+				</li>				
 				<li className={location.pathname.includes('adm16') ? 'active' : ''}>
 					<Link to="/admin/adm_1000/adm1600" >성과관리</Link>
 				</li>
+				{/* 
 				<li className={location.pathname.includes('adm17') ? 'active' : ''}>
 					<Link to="/admin/adm_1000/adm1700" >수요조사관리</Link>
 				</li>
-
+				 */}
+				<li>
+					<button className="ctr" type="button" onClick={(e)=>{toggleHandler(e)}}>수요조사관리</button>
+					<ul className='d2'>
+						{aside.depth.sub7.map((sub) => (
+							<li key={sub.s_id}>
+								<Link to={sub.s_path}>{sub.s_name}</Link>
+							</li>
+						))}
+					</ul>
+				</li>				
 				{/* 
 				{aside.depth2.map((sub) => (
 					<li  key={sub.s_id}>
 						<Link to={sub.s_path} className='no-sub'>{sub.s_name}</Link>
 					</li>
 				))} */}
+				<li className={location.pathname.includes('adm17') ? 'active' : ''}>
+					<Link to="/admin/adm_1000/adm1706" >통계관리</Link>
+				</li>
+
+				
 			</ul>
 		</aside>
 	);
