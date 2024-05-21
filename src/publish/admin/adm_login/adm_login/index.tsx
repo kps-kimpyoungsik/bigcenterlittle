d278cc9ba1777fd5d610ec47ed86cb95626fde6b
spacepublisher <spacepublisher@gmail.com>
2024-05-21 09:53:11 +0900

    관리자 - 로그인 수정

--- src/publish/admin/adm_login/adm_login/index.tsx
+++ src/publish/admin/adm_login/adm_login/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PageContainer from '../../PageContainer';
 // import LayoutAside from "../adm_2000Aside";
 import logo from 'assets/img/logo/logo_login.svg';
+import character from 'assets/img/char/char_login.svg';
 import Button from 'components/buttons/';
 import UIInput from 'components/input/input';
 function ContentsContainer() {
@@ -12,18 +13,20 @@ function ContentsContainer() {
 
 			<div className="adm-login-wrap">
 				<div className="adm-login">
-					<div className="img-area"></div>
+					<div className="img-area">
+						<img src={character} alt=""  className="char" />
+					</div>
 					<div className="form-area">
 						<div className="inner">
-							<img src={logo} alt="대·중소기업 상생협력기금 종합시스템"  className="w190" />
-							<p className="tit tx fs12 fc-pri">ADMIN LOGIN</p>
-							<UIInput className={'tf tf-md w100p'} value={''} placeholder={''} readOnly={false} />
-							<UIInput type="password" className={'tf tf-md w100p'} value={''} placeholder={''} readOnly={false} />
+							<img src={logo} alt="대·중소기업 상생협력기금 종합시스템"  className="w190 mb50" />
+							<p className="tit">ADMIN LOGIN</p>
+							<UIInput className={'tf tf-big w100p'} value={''} placeholder={'아이디를 입력하세요'} readOnly={false} />
+							<UIInput type="password" className={'tf tf-big w100p'} value={''} placeholder={'비밀번호를 입력하세요'} readOnly={false} />
 							<p className="tx tx-alert">아이디를 입력해 주세요.</p>
-							<p className="tx tx-alert">비밀번호를 입력해 주세요.</p>
-							<p className="tx tx-alert">로그인정보가 일치하지 않습니다.</p>
-							<p className="tx tx-alert">비밀번호 5회이상 일치하지 않아 잠겼습니다.</p>
-							<Button color="pri" size="md">로그인</Button>
+							{/* <p className="tx tx-alert">비밀번호를 입력해 주세요.</p> */}
+							{/* <p className="tx tx-alert">로그인정보가 일치하지 않습니다.</p> */}
+							{/* <p className="tx tx-alert">비밀번호 5회이상 일치하지 않아 잠겼습니다.</p> */}
+							<Button color="pri" size="big">로그인</Button>
 						</div>
 						
 					</div>
