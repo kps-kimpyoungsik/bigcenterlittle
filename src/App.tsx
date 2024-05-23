29f9d2148a162ddf16d1729381a89bd44b1af5f0
JI-jeong <jijeong119@gmail.com>
2024-05-23 10:24:42 +0900

    NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,10 @@ function App() {
 		<Suspense fallback={<PageLoading />}>
 			<BrowserRouter>
 				<Switch>
-					<Route path="/main" render={ Main} /> {/* 관리자페이지 */}
-					<Route path="/admin" render={ Admin} /> {/* 사용자페이지 */}
+					<Route path="/main" render={ Main} /> {/* 사용자페이지 */}
+					<Route path="/admin" render={ Admin} /> {/* 관리자페이지 */}
 					<Route path="/guide" render={ Guide} /> {/* 퍼블리셔 관리 파일 */} 
-					{/* <Route path={"*"} component={NotFound}/> */}
+					<Route path={"*"} component={NotFound}/>
 				</Switch>
 				<ToastContainer
 					position="top-right"
--- src/publish/notfound/index.tsx
+++ src/publish/notfound/index.tsx
@@ -1,14 +1,18 @@
 
 import React from "react";
 // import PageContainer from '../main/PageContainer';
-import MainHeader from "layouts/main/MainHeader";
-import Button from 'components/buttons/';
+// import MainHeader from "layouts/main/MainHeader";
+// import Button from 'components/buttons/';
+import Notfound from 'publish/admin/exceptionpage/404';
 function NotFoundContainer() {
 	return (
 		<div className="fww-root">
+			<Notfound />
+			{/* 
 			404, Syscheck퍼블은 다음 참고 <br/>
 			src\publish\admin\exceptionpage\404\index.tsx <br/>
-			src\publish\admin\exceptionpage\syscheck\index.tsx
+			src\publish\admin\exceptionpage\syscheck\index.tsx 
+			*/}
 		</div>
 
 	);
