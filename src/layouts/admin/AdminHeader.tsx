c533f073a0195ff26489b3569b60813b6571237c
JI-jeong <jijeong119@gmail.com>
2024-05-24 15:05:41 +0900

    gnb수정

--- src/layouts/admin/AdminHeader.tsx
+++ src/layouts/admin/AdminHeader.tsx
@@ -33,35 +33,31 @@ function LayoutHeader() {
 				</ul>
 			</div>
 			<header className={hidden ? 'fww-header adm' : 'fww-header adm active'}>
-				<div className='left-area'>
-					<Link to="/admin/home" className='logo'>
-						<img src={logo} alt="대·중소기업 상생협력기금 종합시스템" />
-					</Link>
-				</div>
-				<div className='right-area'>
-					<nav 
-						className='fww-gnb' 
-						onMouseEnter={() => setHidden(false)}
-						onMouseLeave={() => setHidden(true)}
-					>
-						<ul>
-							{menutree.gnb.map((gnb, index) => (
-								<li key={index} className={location.pathname.includes(gnb.path.slice(0,8)) ? 'd1 active' : 'd1'}>
-									<NavLink to={"/admin/" + gnb.path }>
-										<span>{gnb.name}</span>
-										</NavLink>
-									<ul className='d2' >
-										{gnb.sub?.map((depth2) => (
-											<li key={depth2.s_id} className={location.pathname.slice(-4,23) === depth2.sceenid? 'active' : ''}>
-												<Link to={"/admin/" + gnb.path.slice(0,8) + "/adm" + depth2.sceenid}>{depth2.name}</Link>	
-											</li>
-										))}
-									</ul>
-								</li>
-							))}
-						</ul>
-					</nav>
-				</div>
+				<Link to="/admin/home" className='logo'>
+					<img src={logo} alt="대·중소기업 상생협력기금 종합시스템" />
+				</Link>
+				<nav 
+					className='fww-gnb' 
+					onMouseEnter={() => setHidden(false)}
+					onMouseLeave={() => setHidden(true)}
+				>
+					<ul>
+						{menutree.gnb.map((gnb, index) => (
+							<li key={index} className={location.pathname.includes(gnb.path.slice(0,8)) ? 'd1 active' : 'd1'}>
+								<NavLink to={"/admin/" + gnb.path }>
+									<span>{gnb.name}</span>
+									</NavLink>
+								<ul className='d2' >
+									{gnb.sub?.map((depth2) => (
+										<li key={depth2.s_id} className={location.pathname.slice(-4,23) === depth2.sceenid? 'active' : ''}>
+											<Link to={"/admin/" + gnb.path.slice(0,8) + "/adm" + depth2.sceenid}>{depth2.name}</Link>	
+										</li>
+									))}
+								</ul>
+							</li>
+						))}
+					</ul>
+				</nav>
 			</header>			
 
 		</>
--- src/layouts/main/MainHeader.tsx
+++ src/layouts/main/MainHeader.tsx
@@ -33,35 +33,39 @@ function LayoutHeader() {
 				</ul>
 			</div>
 			<header className={hidden ? 'fww-header usr' : 'fww-header usr active'}>
-				<div className='left-area'>
-					<Link to="/main" className='logo'>
+				<Link to="/main" className='logo'>
 						<img src={logo} alt="대·중소기업 상생협력기금 종합시스템" />
-					</Link>
+				</Link>
+				<nav
+					className='fww-gnb' 
+					onMouseEnter={() => setHidden(false)}
+					onMouseLeave={() => setHidden(true)}
+				>
+					<ul>
+						{menutree.gnb.map((gnb, index) => (
+						<li key={index} className={location.pathname.includes(gnb.path.slice(0,8)) ? 'd1 active' : 'd1'}>
+							<NavLink to={"/main/" + gnb.path }>
+								<span>{gnb.name}</span>
+								</NavLink>
+							<ul className='d2' >
+								{gnb.sub.map((depth2) => (
+								<li key={depth2.s_id} className={location.pathname.slice(-4,23) === depth2.sceenid? 'active' : ''}>
+									<Link to={"/main/" + gnb.path.slice(0,8) + "/usr_" + depth2.sceenid}>{depth2.name}</Link>
+								</li>
+								))}
+							</ul>
+						</li>
+						))}
+					</ul>
+				</nav>
+
+{/* 
+				<div className='left-area'>
+					
 				</div>
 				<div className='right-area'>
-					<nav
-						className='fww-gnb' 
-						onMouseEnter={() => setHidden(false)}
-						onMouseLeave={() => setHidden(true)}
-					>
-						<ul>
-							{menutree.gnb.map((gnb, index) => (
-							<li key={index} className={location.pathname.includes(gnb.path.slice(0,8)) ? 'd1 active' : 'd1'}>
-								<NavLink to={"/main/" + gnb.path }>
-									<span>{gnb.name}</span>
-									</NavLink>
-								<ul className='d2' >
-									{gnb.sub.map((depth2) => (
-									<li key={depth2.s_id} className={location.pathname.slice(-4,23) === depth2.sceenid? 'active' : ''}>
-										<Link to={"/main/" + gnb.path.slice(0,8) + "/usr_" + depth2.sceenid}>{depth2.name}</Link>
-									</li>
-									))}
-								</ul>
-							</li>
-							))}
-						</ul>
-					</nav>
-				</div>
+					
+				</div> */}
 			</header>
 		</>
 	);
